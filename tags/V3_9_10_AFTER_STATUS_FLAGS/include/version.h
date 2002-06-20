71f18db78e7f955c1ed483edaeafee8f4432bb26
nobody <nobody@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2002-06-20 19:12:52 +0000

    This commit was manufactured by cvs2svn to create tag 'V3_9_10_AFTER_STATUS_FLAGS'.

---
+++ tags/V3_9_10_AFTER_STATUS_FLAGS/include/version.h
@@ -0,0 +1,6 @@
+#define DEBUG 1
+#define AVERSION 3
+#define VERSION "3.9.10"
+#define VSTRING "3910"
+#define DATE    "2 June 2002"
+#define LSMDATE "02Jun02"
---
+++ tags/V3_9_10_AFTER_STATUS_FLAGS/src/lib/apcerror.c
@@ -0,0 +1,99 @@
+/*
+ *  apcerror.c -- error functions.
+ *
+ *  apcupsd.c	     -- Simple Daemon to catch power failure signals from a
+ *		     BackUPS, BackUPS Pro, or SmartUPS (from APCC).
+ *		  -- Now SmartMode support for SmartUPS and BackUPS Pro.
+ *
+ *  Copyright (C) 1996-99 Andre M. Hedrick <andre@suse.com>
+ *  Copyright (C) 1999-2000 Riccardo Facchetti <riccardo@master.oasi.gpa.it>
+ *  All rights reserved.
+ *
+ */
+
+/*
+ *			 GNU GENERAL PUBLIC LICENSE
+ *			    Version 2, June 1991
+ *
+ *  Copyright (C) 1989, 1991 Free Software Foundation, Inc.
+ *			       675 Mass Ave, Cambridge, MA 02139, USA
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/*
+ *  IN NO EVENT SHALL ANY AND ALL PERSONS INVOLVED IN THE DEVELOPMENT OF THIS
+ *  PACKAGE, NOW REFERRED TO AS "APCUPSD-Team" BE LIABLE TO ANY PARTY FOR
+ *  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING
+ *  OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF ANY OR ALL
+ *  OF THE "APCUPSD-Team" HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  THE "APCUPSD-Team" SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
+ *  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ *  ON AN "AS IS" BASIS, AND THE "APCUPSD-Team" HAS NO OBLIGATION TO PROVIDE
+ *  MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *  THE "APCUPSD-Team" HAS ABSOLUTELY NO CONNECTION WITH THE COMPANY
+ *  AMERICAN POWER CONVERSION, "APCC".  THE "APCUPSD-Team" DID NOT AND
+ *  HAS NOT SIGNED ANY NON-DISCLOSURE AGREEMENTS WITH "APCC".  ANY AND ALL
+ *  OF THE LOOK-A-LIKE ( UPSlink(tm) Language ) WAS DERIVED FROM THE
+ *  SOURCES LISTED BELOW.
+ *
+ */
+
+#include "apc.h"
+
+void error_cleanup(char *buf);
+
+/*********************************************************************
+ * subroutine normally called by macro error_abort() to print
+ *  FATAL ERROR message and supplied error message
+ *
+ */
+void error_out(char *file, int line, char *fmt, ...)
+{
+    char buf[256];
+    va_list arg_ptr;
+    int i;
+
+    sprintf(buf, "FATAL ERROR in %s at line %d\n", file, line);
+    i = strlen(buf);
+    va_start(arg_ptr, fmt);
+    vsprintf((char *)&buf[i], (char *)fmt, arg_ptr);
+    va_end(arg_ptr);
+    fprintf(stdout, buf);
+
+    error_cleanup(buf);
+    exit(1);
+}
+
+/* simply print the message and exit */
+void error_exit(char *fmt, ...)
+{
+    va_list arg_ptr;
+    char buf[256];
+
+    va_start(arg_ptr, fmt);
+    vsprintf(buf, (char *)fmt, arg_ptr);
+    va_end(arg_ptr);
+    fprintf(stdout, buf);
+
+    error_cleanup(buf);
+    exit(1);
+}
