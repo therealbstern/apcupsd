31d5218ef5bfc39b112da4e8179c9bc5d1399da7
rfacchetti <rfacchetti@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2002-06-30 18:08:31 +0000

    Time to bump up the date of version.h

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.9.10"
 #define VSTRING "3910"
-#define DATE    "2 June 2002"
-#define LSMDATE "02Jun02"
+#define DATE    "30 June 2002"
+#define LSMDATE "30Jun02"
