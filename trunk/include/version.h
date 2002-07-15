f65e3117d502221684505579dc8337ff5467f1a8
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2002-07-15 19:14:10 +0000

    See kes15Jul02; doc, 940-0128 cable, apctest

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.9.10"
 #define VSTRING "3910"
-#define DATE    "30 June 2002"
-#define LSMDATE "30Jun02"
+#define DATE    "15 July 2002"
+#define LSMDATE "15Jul02"
