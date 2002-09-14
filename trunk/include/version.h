42797214d9ce565411b7e343d38790419236a53f
rfacchetti <rfacchetti@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2002-09-14 12:03:19 +0000

    Fixed autoconf check and dependances of -lpanel,etc with -lncurses. Made more portable the apccontrol external scripts when calling the   mailer (subject is now in the echo lines instead of relying on the   presence of a -s switch on the mailer). Added gentoo platform. Added DESTDIR variable for platform packaging. Fixed a off-by-one problem in events table.

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.1"
 #define VSTRING "3101"
-#define DATE    "06 September 2002"
-#define LSMDATE "06Sepl02"
+#define DATE    "14 September 2002"
+#define LSMDATE "14Sep02"
