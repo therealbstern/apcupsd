2730a527381122fd77f6a6a5d7048e5e6cc81196
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2002-12-11 22:29:24 +0000

    Fix missing semicolon

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.3"
 #define VSTRING "3103"
-#define DATE    "11 December 2002"
-#define LSMDATE "11Dec02"
+#define DATE    "12 December 2002"
+#define LSMDATE "12Dec02"
