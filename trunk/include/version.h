c15f3f9a63624bec3ed2426b90b58ce50f841a44
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-01-29 10:16:55 +0000

    Document updates

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.4"
 #define VSTRING "3104"
-#define DATE    "19 January 2003"
-#define LSMDATE "19Jan03"
+#define DATE    "21 January 2003"
+#define LSMDATE "21Jan03"
