93aec2d458f1f06702cbeb90aa08ae327ff412e5
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-02-02 22:07:48 +0000

    Fix master-slave config, add apcupsd.spec for Mandrake

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.4"
 #define VSTRING "3104"
-#define DATE    "28 January 2003"
-#define LSMDATE "28Jan03"
+#define DATE    "02 February 2003"
+#define LSMDATE "02Feb03"
