c5c5efba18d96b77ef0cd288b9901eb98dcb6393
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-02-04 13:02:45 +0000

    Update document

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.5"
 #define VSTRING "3105"
-#define DATE    "03 February 2003"
-#define LSMDATE "03Feb03"
+#define DATE    "04 February 2003"
+#define LSMDATE "04Feb03"
