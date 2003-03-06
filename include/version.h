78d23df2faa46ef72249cfbc4ff8a7455cd7d860
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-03-06 08:58:19 +0000

    TCP WRAPPERS fix

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.6"
 #define VSTRING "3106"
-#define ADATE   "21 February 2003"
-#define LSMDATE "21Feb03"
+#define ADATE   "06 March 2003"
+#define LSMDATE "06Mar03"
