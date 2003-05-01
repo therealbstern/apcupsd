a1c1bf8c86d2258ca18ae5d95309f27a816d9cf9
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-05-01 08:26:00 +0000

    Update ChangeLog

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.6"
 #define VSTRING "3106"
-#define ADATE   "22 April 2003"
-#define LSMDATE "22Apr03"
+#define ADATE   "01 May 2003"
+#define LSMDATE "01May03"
