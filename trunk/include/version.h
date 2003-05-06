7ee53270f1e383be6f29edf646077c5bed7ac67c
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-05-06 16:01:31 +0000

    Correct ./configure output for missing libgd

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.6"
 #define VSTRING "3106"
-#define ADATE   "01 May 2003"
-#define LSMDATE "01May03"
+#define ADATE   "06 May 2003"
+#define LSMDATE "06May03"
