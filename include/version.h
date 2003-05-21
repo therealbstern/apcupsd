1d48cd1c97020d6c5286bbee9bdfbf106e49fadb
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-05-21 07:24:47 +0000

    Correct DESTDIR typo

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.6"
 #define VSTRING "3106"
-#define ADATE   "18 May 2003"
-#define LSMDATE "18May03"
+#define ADATE   "20 May 2003"
+#define LSMDATE "20May03"
