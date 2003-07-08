a387e33c6c32615a3354ab3155555af666a232ec
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-07-08 12:31:12 +0000

    Add new files + Sergey's kernel patch

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.6"
 #define VSTRING "3106"
-#define ADATE   "07 July 2003"
-#define LSMDATE "07Jul03"
+#define ADATE   "08 July 2003"
+#define LSMDATE "08Jul03"
