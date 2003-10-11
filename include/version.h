57941eeeb37e90aa7b27a70281e10cc68fe91e26
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-10-11 09:45:20 +0000

    Final Release 3.10.6 doc updates

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.6"
 #define VSTRING "3106"
-#define ADATE   "28 September 2003"
-#define LSMDATE "28Sep03"
+#define ADATE   "10 October 2003"
+#define LSMDATE "10Oct03"
