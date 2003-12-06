7abd7b9166ce9020046814133769c610ebabd262
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-12-06 21:24:47 +0000

    2.6 USB fix by Reg. Clemens + doc updates

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.8"
 #define VSTRING "3108"
-#define ADATE   "26 November 2003"
-#define LSMDATE "26Nov03"
+#define ADATE   "06 December 2003"
+#define LSMDATE "06Dec03"
