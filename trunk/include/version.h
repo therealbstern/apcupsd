eed124a8e07e059510ab57c4601427958baf0685
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-12-18 12:12:17 +0000

    Fix Makefile dependencies + Slackware rc6 patch

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.8"
 #define VSTRING "3108"
-#define ADATE   "06 December 2003"
-#define LSMDATE "06Dec03"
+#define ADATE   "18 December 2003"
+#define LSMDATE "18Dec03"
