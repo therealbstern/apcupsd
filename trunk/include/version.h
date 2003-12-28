605c9236b5d5c7faf6f8078ca7ebbe027ddaeff8
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2003-12-28 09:51:29 +0000

    misc

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.8"
 #define VSTRING "3108"
-#define ADATE   "18 December 2003"
-#define LSMDATE "18Dec03"
+#define ADATE   "28 December 2003"
+#define LSMDATE "28Dec03"
