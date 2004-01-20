4b499cf8d5ab5264b295d38162b884c28c4b85a0
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2004-01-20 15:35:55 +0000

    Fix bad html in multimoncss.c + new Mandrake spec from David Walser

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -1,6 +1,6 @@
 #define DEBUG 1
 #define AVERSION 3
-#define VERSION "3.10.10"
-#define VSTRING "31010"
-#define ADATE   "16 January 2004"
-#define LSMDATE "16Jan04"
+#define VERSION "3.10.11"
+#define VSTRING "31011"
+#define ADATE   "20 January 2004"
+#define LSMDATE "20Jan04"
