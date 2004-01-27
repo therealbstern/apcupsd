293a0bad1172c85dd7ef2b6d28341040ec7c851a
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2004-01-27 11:41:26 +0000

    Doc + use old master/slave code

--- trunk/include/version.h
+++ trunk/include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.11"
 #define VSTRING "31011"
-#define ADATE   "20 January 2004"
-#define LSMDATE "20Jan04"
+#define ADATE   "26 January 2004"
+#define LSMDATE "26Jan04"
