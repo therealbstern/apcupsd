e314fff8bb5745516aae1fb9cb01d8f05dd2a8bc
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2004-02-12 18:16:09 +0000

    Eliminate unused code in snmp directory

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.12"
 #define VSTRING "31012"
-#define ADATE   "07 February 2004"
-#define LSMDATE "07Feb04"
+#define ADATE   "12 February 2004"
+#define LSMDATE "12Feb04"
