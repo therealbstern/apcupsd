542a474d78473b55545fcc34f9c408ab4dd99004
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2004-02-23 21:06:41 +0000

    Misc

--- include/version.h
+++ include/version.h
@@ -2,5 +2,5 @@
 #define AVERSION 3
 #define VERSION "3.10.12"
 #define VSTRING "31012"
-#define ADATE   "12 February 2004"
-#define LSMDATE "12Feb04"
+#define ADATE   "21 February 2004"
+#define LSMDATE "21Feb04"
