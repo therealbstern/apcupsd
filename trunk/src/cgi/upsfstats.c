bfa3b92cbca782258e39ea72736513a5571a5b7b
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2004-02-24 17:08:39 +0000

    New patch from William King <wrking@dadaboom.com> applied

--- trunk/src/cgi/upsfstats.c
+++ trunk/src/cgi/upsfstats.c
@@ -14,13 +14,11 @@
 #include <sys/time.h>
 
 #include "cgiconfig.h"
+#include "cgilib.h"
 #include "upsfetch.h"
 
-char   monhost[256];
-char   answer[256];
-
-extern int extractcgiargs();
-extern int checkhost(char *host);
+static char   monhost[256];
+static char   answer[256];
 
 void parsearg(char var[255], char value[255]) 
 {
@@ -29,55 +27,57 @@ void parsearg(char var[255], char value[255])
     }
 }
 
-static void bailout()
+static void finish(void)
 {
-    printf ("</BODY></HTML>\n");
-    exit(0);
-}
-
-
-int main() 
-{
-    strcpy (monhost, "127.0.0.1");  /* default host */
-
-    printf ("Content-type: text/html\n");
-    printf ("\n");
-
-    printf ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n");
-    printf ("<HTML>\n");
-    printf ("<HEAD><TITLE>APCUPSD STATUS Output Page</TITLE></HEAD>\n");
-    printf ("<BODY BGCOLOR=\"#FFFFFF\">\n");
-
-    if (!extractcgiargs()) {
-        printf("Unable to extract cgi arguments!\n");
-	bailout();
-    }
-
-    if (!checkhost(monhost)) {
-        printf ("Access to %s host is not authorized.\n", monhost);
-	bailout();
-    }
-
-    /* check if host is available */
-    if (getupsvar (monhost, "date", answer, sizeof(answer)) <= 0)  {
-        printf ("Host %s is not available.\n", monhost);
-	bailout();
-    }
-    printf ("<blockquote><pre>");
-
-    printf (statbuf);
-
-    printf ("</pre></blockquote>\n");
-
 #ifdef VALIDATE_HTML
-    printf ("<div><small>\n");
+    printf ("<hr /><div><small>\n");
     printf ("<a href=\"http://jigsaw.w3.org/css-validator/check/referer\">\n");
     printf ("<img style=\"float:right\" src=\"/icons/vcss\" alt=\"Valid CSS!\" height=\"31\" width=\"88\"/></a>\n");
     printf("<a href=\"http://validator.w3.org/check/referer\">\n");
     printf("<img style=\"float:right\" src=\"/icons/valid-xhtml10\" alt=\"Valid XHTML 1.0!\" height=\"31\" width=\"88\"/></a>\n");
     printf ("</small></div>\n");
 #endif
-    printf ("</BODY></HTML>\n");
+    printf ("</body></html>\n");
+    exit(0);
+}
 
+int main(int argc, char **argv) 
+{
+    strcpy (monhost, "127.0.0.1");  /* default host */
+
+    printf ("Content-type: text/html\n\n");
+
+    printf("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n");
+    printf("  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n");
+    printf("<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">\n");
+    printf ("<head>\n");
+    printf ("<title>APCUPSD STATUS Output Page</title>\n");
+    printf ("<style type=\"text/css\">\n");
+    printf ("  body {color: black; background: #ffffff}\n");
+    printf ("</style>\n");
+    printf ("</head>\n");
+    printf ("<body>\n");
+
+    if (!extractcgiargs()) {
+        printf("<p>Unable to extract cgi arguments!</p>\n");
+	finish();
+    }
+
+    if (!checkhost(monhost)) {
+        printf ("<p>Access to %s host is not authorized.</p>\n", monhost);
+	finish();
+    }
+
+    /* check if host is available */
+    if (getupsvar (monhost, "date", answer, sizeof(answer)) <= 0)  {
+        printf ("<p>Host %s is not available.</p>\n", monhost);
+	finish();
+    }
+    printf ("<blockquote><pre>");
+
+    printf (statbuf);
+
+    printf ("</pre></blockquote>\n");
+    finish();
     exit(0);
 }
