acc870b0e56fef96fa4dbb34df2946ee85d54f9c
wrking <wrking@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2004-03-13 04:32:31 +0000

    Added html_finish() function.

--- trunk/src/cgi/cgilib.c
+++ trunk/src/cgi/cgilib.c
@@ -134,3 +134,20 @@ void html_puts(const unsigned char *p)
         p++;
     }
 }
+
+/*
+ * Print the standard footer which appears on every html page and close out
+ * the html document.
+ */
+void html_finish(void)
+{
+#ifdef VALIDATE_HTML
+    puts ("<hr /><div><small>");
+    puts ("<a href=\"http://jigsaw.w3.org/css-validator/check/referer\">");
+    puts ("<img style=\"float:right\" src=\"http://jigsaw.w3.org/css-validator/images/vcss\" alt=\"Valid CSS!\" height=\"31\" width=\"88\"/></a>");
+    puts ("<a href=\"http://validator.w3.org/check/referer\">");
+    puts ("<img style=\"float:right\" src=\"http://www.w3.org/Icons/valid-xhtml10\" alt=\"Valid XHTML 1.0!\" height=\"31\" width=\"88\"/></a>");
+    puts ("</small></div>");
+#endif
+    puts ("</body></html>");
+}
