79e01642f113180c51556e43c9fe3c5592bf55d6
karlec <karlec@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2005-04-01 16:18:31 +0000

    Added texfix Makefile target to run fix_tex.pl. Changed fix_tex.pl to only indent lines following \item commands, and to    ignore prior passes.

--- trunk/doc/latex/fix_tex.pl
+++ trunk/doc/latex/fix_tex.pl
@@ -1,92 +1,124 @@
 #!/usr/bin/perl -w
-#
-#Modifies manual.tex to fix up several things:
-#
-#1. Changes section, subsection, etc. according to %mapping
-#2. Changes the same things inside \addcontentsline commands.
-#3. Add three spaces to the two lines following \item commands.
+# Finds multiple hyphens not inside a verbatim environment (or \verb).  
+#   Places these inside a \verb{} contruct so they will not be converted
+#   to single hyphen by latex or latex2html.
 
 use strict;
 
-my $INPUTFILE = "manual.tex";
-my $OUTPUTFILE = "manual.tex";
+my %args;
 
-my %mapping = ( 'section' => 'section',
-			'subsection' => 'section',
-			'subsubsection' => 'subsection',
-			'paragraph' => 'subsubsection',
-			'subparagraph' => 'paragraph',
-			);
-#\addcontentsline{toc}{subsubsection}
 
-my $sectionsearch = "\\\\(";
-my $tocsearch = "\\\\addcontentsline\\{toc\\}\\{(";
-my $insidesearch = "(";
-foreach (keys(%mapping)) {
-	$sectionsearch .= "$_|";
-	$tocsearch .= "$_|";
-	$insidesearch .= "$_|";
-}
-$sectionsearch =~ s/\|$/)\\*?\\{/;
-$tocsearch =~ s/\|$/)\\}/;
-$insidesearch =~ s/\|$/)/;
+sub get_includes {
+	# Get a list of include files from the top-level tex file.
+	my (@list,$file);
+	
+	foreach my $filename (@_) {
+		$filename or next;
+		# Start with the top-level latex file so it gets checked too.
+		push (@list,$filename);
 
-# Read in the contents of the file.
-my $text = "";
-open IF, "<$INPUTFILE" or die "cannot open $INPUTFILE for reading";
-while (<IF>) {
-	$text .= $_;
-}
-print length($text)," bytes loaded\n";
-close IF;
+		# Get a list of all the html files in the directory.
+		open IF,"<$filename" or die "Cannot open input file $filename";
+		while (<IF>) {
+			chomp;
+			push @list,"$1.tex" if (/\\include\{(.*?)\}/);
+		}
 
-# Now process the text.
-my $output = "";
-my $this;
-my $sectcnt = 0;
-while ($text =~ /$sectionsearch/s) {
-	$output .= $`;
-	$text = $';
-	$this = $&;
-	$sectcnt += $this =~ s/$insidesearch/$mapping{$1}/;
-	$output .= $this;
-}
-$text = $output . $text;
-
-$output = "";
-my $toccnt = 0;
-while ($text =~ /$tocsearch/s) {
-	$output .= $`;
-	$text = $';
-	$this = $&;
-	$toccnt += $this =~ s/$insidesearch/$mapping{$1}/;
-	$output .= $this;
-}
-
-$text = $output . $text;
-
-# Next we look for a line that starts with \item, and indent the two next lines (if not blank)
-#  by three spaces.
-my $linecnt = 3;
-my $itemcnt = 0;
-$output = "";
-foreach (split(/\n/,$text)) {
-	$_ .= "\n"; # Put back the return.
-	if ($linecnt < 3 and /^[^\n]/ and !/^\\item/) {
-		$output .= "   " . $_;
-		$itemcnt++;
-		$linecnt++;
-	} else {
-		$linecnt = 3;
-		$output .= $_;
+		close IF;
 	}
-	/^\\item / and $linecnt = 1;
+	return @list;
 }
-		
-open OF,">$OUTPUTFILE" or die "Cannot open $OUTPUTFILE for writing";
-print OF $output;
-close OF;
-print length($output)," bytes written\n";
-print "$sectcnt sections changed.  $toccnt toc entries changed.\n";
-print "Indented $itemcnt lines after \\item.\n";
 
+sub convert_files {
+	my (@files) = @_;
+	my ($linecnt,$filedata,$output,$thiscnt,$cnt);
+	
+	# Build the test string to check for the various environments.
+	#   We only do the conversion if the multiple hyphens are outside of a 
+	#   verbatim environment (either \begin{verbatim}...\end{verbatim} or 
+	#   \verb{--}).  Capture those environments and pass them to the output
+	#   unchanged.
+
+	$cnt = 0;
+	foreach my $file (@files) {
+		# Open the file and load the whole thing into $filedata. A bit wasteful but
+		#   easier to deal with, and we don't have a problem with speed here.
+		$filedata = "";
+		open IF,"<$file" or die "Cannot open input file $file";
+		while (<IF>) {
+			$filedata .= $_;
+		}
+		close IF;
+		
+		# Set up to process the file data.
+		$thiscnt = 0;
+
+		# We look for a line that starts with \item, and indent the two next lines (if not blank)
+		#  by three spaces.
+		my $linecnt = 3;
+		my $itemcnt = 0;
+		$output = "";
+		foreach (split(/\n/,$filedata)) {
+			$_ .= "\n"; # Put back the return.
+			# If this line is less than the third line past the \item command,
+			#  and the line isn't blank and doesn't start with whitespace
+			#  add three spaces to the start of the line. Keep track of the number
+			#  of lines changed.
+			if ($linecnt < 3 and !/^\\item/) {
+				if (/^[^\n\s]/) {
+					$output .= "   " . $_;
+					$thiscnt++;
+				} else {
+					$output .= $_;
+				}
+				$linecnt++;
+			} else {
+				$linecnt = 3;
+				$output .= $_;
+			}
+			/^\\item / and $linecnt = 1;
+		}
+	
+		# If any hyphens were converted, save the file.
+		if ($thiscnt) {
+			open OF,">$file" or die "Cannot open output file $file";
+			print OF $output;
+			close OF;
+			print "$thiscnt line", ($thiscnt == 1) ? "" : "s"," Changed in $file\n";
+		}
+
+		$cnt += $thiscnt;
+	}
+	return $cnt;
+}
+
+sub check_arguments {
+	# Checks command-line arguments for ones starting with --  puts them into
+	#   a hash called %args and removes them from @ARGV.
+	my $args = shift;
+	my $i;
+
+	for ($i = 0; $i < $#ARGV; $i++) {
+		$ARGV[$i] =~ /^\-+/ or next;
+		$ARGV[$i] =~ s/^\-+//;
+		$args{$ARGV[$i]} = "";
+		delete ($ARGV[$i]);
+		
+	}
+}
+
+##################################################################
+#                       MAIN                                  ####
+##################################################################
+
+my @includes;
+my $cnt;
+
+check_arguments(\%args);
+
+# Examine the file pointed to by the first argument to get a list of 
+#  includes to test.
+@includes = get_includes(@ARGV);
+
+$cnt = convert_files(@includes);
+$cnt or print "No lines changed\n";
