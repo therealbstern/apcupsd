d2d387b1a2e0f9c89b4ffb83399d4645afe224df
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2005-05-30 16:10:23 +0000

    Code style normalization.

--- trunk/src/drivers/snmp/drv_rfc1628.c
+++ trunk/src/drivers/snmp/drv_rfc1628.c
@@ -1,117 +1,90 @@
 /*
- *  drv_rfc1628.c -- rfc1628 aka UPS-MIB driver
- *  Copyright (C) 1999-2002 Riccardo Facchetti <riccardo@apcupsd.org>
+ * drv_rfc1628.c
  *
- *  apcupsd.c   -- Simple Daemon to catch power failure signals from a
- *                 BackUPS, BackUPS Pro, or SmartUPS (from APCC).
- *              -- Now SmartMode support for SmartUPS and BackUPS Pro.
- *
+ * rfc1628 aka UPS-MIB driver
  */
 
 /*
-   Copyright (C) 2000-2004 Kern Sibbald
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public
-   License along with this program; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-   MA 02111-1307, USA.
-
+ * Copyright (C) 2000-2004 Kern Sibbald
+ * Copyright (C) 1999-2002 Riccardo Facchetti <riccardo@apcupsd.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
  */
 
 #include "apc.h"
 #include "snmp.h"
 #include "snmp_private.h"
 
-static int rfc_1628_check_alarms(UPSINFO *ups) {
-    struct snmp_ups_internal_data *Sid = (struct snmp_ups_internal_data *)ups->driver_internal_data;
-    struct snmp_session *s = &Sid->session;
-    ups_mib_t *data = (ups_mib_t *)Sid->MIB;
+static int rfc_1628_check_alarms(UPSINFO *ups)
+{
+   struct snmp_ups_internal_data *Sid =
+      (struct snmp_ups_internal_data *)ups->driver_internal_data;
+   struct snmp_session *s = &Sid->session;
+   ups_mib_t *data = (ups_mib_t *)Sid->MIB;
 
-    /*
-     * Check the Ethernet COMMLOST first, then check the
-     * Agent/SNMP->UPS serial COMMLOST together with all the other
-     * alarms.
-     */
-    if (ups_mib_mgr_get_upsAlarmEntry(s, &(data->upsAlarmEntry)) == -1) {
-        set_ups(UPS_COMMLOST);
-        free(data->upsAlarmEntry);
-        return 0;
-    } else {
-        clear_ups(UPS_COMMLOST);
-    }
-    free(data->upsAlarmEntry);
-    return 1;
+   /*
+    * Check the Ethernet COMMLOST first, then check the
+    * Agent/SNMP->UPS serial COMMLOST together with all the other
+    * alarms.
+    */
+   if (ups_mib_mgr_get_upsAlarmEntry(s, &(data->upsAlarmEntry)) == -1) {
+      set_ups(UPS_COMMLOST);
+      free(data->upsAlarmEntry);
+      return 0;
+   } else {
+      clear_ups(UPS_COMMLOST);
+   }
+
+   free(data->upsAlarmEntry);
+   return 1;
 }
 
-int rfc1628_snmp_kill_ups_power(UPSINFO *ups) {
-/*
- *  struct snmp_ups_internal_data *Sid = ups->driver_internal_data;
- *  struct snmp_session *s = &Sid->session;
- *  ups_mib_t *data = Sid->MIB;
- */
-
-    return 0;
+int rfc1628_snmp_kill_ups_power(UPSINFO *ups)
+{
+   return 0;
 }
 
-int rfc1628_snmp_ups_get_capabilities(UPSINFO *ups) {
-/*
- *  struct snmp_ups_internal_data *Sid = ups->driver_internal_data;
- *  struct snmp_session *s = &Sid->session;
- *  ups_mib_t *data = Sid->MIB;
- */
-    int i = 0;
+int rfc1628_snmp_ups_get_capabilities(UPSINFO *ups)
+{
+   int i = 0;
 
-    /*
-     * Assume that an UPS with SNMP control has all the capabilities.
-     * We know that the RFC1628 doesn't even implement some of the
-     * capabilities. We do this way for sake of simplicity.
-     */
-    for (i = 0; i <= CI_MAX_CAPS; i++) {
-        ups->UPS_Cap[i] = TRUE;
-    }
+   /*
+    * Assume that an UPS with SNMP control has all the capabilities.
+    * We know that the RFC1628 doesn't even implement some of the
+    * capabilities. We do this way for sake of simplicity.
+    */
+   for (i = 0; i <= CI_MAX_CAPS; i++)
+      ups->UPS_Cap[i] = TRUE;
 
-    return 1;
+   return 1;
 }
 
-int rfc1628_snmp_ups_read_static_data(UPSINFO *ups) {
-/*
- *  struct snmp_ups_internal_data *Sid = ups->driver_internal_data;
- *  struct snmp_session *s = &Sid->session;
- *  ups_mib_t *data = Sid->MIB;
- */
-
-    rfc_1628_check_alarms(ups);
-    return 1;
+int rfc1628_snmp_ups_read_static_data(UPSINFO *ups)
+{
+   rfc_1628_check_alarms(ups);
+   return 1;
 }
 
-int rfc1628_snmp_ups_read_volatile_data(UPSINFO *ups) {
-/*
- *  struct snmp_ups_internal_data *Sid = ups->driver_internal_data;
- *  struct snmp_session *s = &Sid->session;
- *  ups_mib_t *data = Sid->MIB;
- */
-
-    rfc_1628_check_alarms(ups);
-    return 1;
+int rfc1628_snmp_ups_read_volatile_data(UPSINFO *ups)
+{
+   rfc_1628_check_alarms(ups);
+   return 1;
 }
 
-int rfc1628_snmp_ups_check_state(UPSINFO *ups) {
-/*
- *  struct snmp_ups_internal_data *Sid = ups->driver_internal_data;
- *  struct snmp_session *s = &Sid->session;
- *  ups_mib_t *data = Sid->MIB;
- */
-
-    rfc_1628_check_alarms(ups);
-    return 1;
+int rfc1628_snmp_ups_check_state(UPSINFO *ups)
+{
+   rfc_1628_check_alarms(ups);
+   return 1;
 }
--- trunk/src/drivers/snmp/snmp.h
+++ trunk/src/drivers/snmp/snmp.h
@@ -1,32 +1,26 @@
 /*
- *  snmp.h  -- public header file for this driver
+ * snmp.h
  *
- *  apcupsd.c -- Simple Daemon to catch power failure signals from a
- *               BackUPS, BackUPS Pro, or SmartUPS (from APCC).
- *            -- Now SmartMode support for SmartUPS and BackUPS Pro.
- *
- *  All rights reserved.
- *
+ * Public header for the SNMP UPS driver
  */
 
 /*
-   Copyright (C) 2000-2004 Kern Sibbald
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public
-   License along with this program; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-   MA 02111-1307, USA.
-
+ * Copyright (C) 2000-2004 Kern Sibbald
+ * Copyright (C) 1999-2001 Riccardo Facchetti <riccardo@apcupsd.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
  */
 
 #ifndef _SNMP_H
@@ -34,25 +28,24 @@
 
 #ifdef HAVE_UCD_SNMP
 
-#undef UCD_COMPATIBLE
-#define UCD_COMPATIBLE 1
+# undef UCD_COMPATIBLE
+# define UCD_COMPATIBLE 1
 
-#include <ucd-snmp/asn1.h>
-#include <ucd-snmp/snmp.h>
-#include <ucd-snmp/snmp_api.h>
-#include <ucd-snmp/snmp_client.h>
+# include <ucd-snmp/asn1.h>
+# include <ucd-snmp/snmp.h>
+# include <ucd-snmp/snmp_api.h>
+# include <ucd-snmp/snmp_client.h>
 
 #else
 
-#ifdef HAVE_NET_SNMP
-#include <net-snmp/library/asn1.h>
-#include <net-snmp/library/snmp.h>
-#include <net-snmp/library/snmp_api.h>
-#include <net-snmp/library/snmp_client.h>
-#endif
- 
-#endif
+# ifdef HAVE_NET_SNMP
+#  include <net-snmp/library/asn1.h>
+#  include <net-snmp/library/snmp.h>
+#  include <net-snmp/library/snmp_api.h>
+#  include <net-snmp/library/snmp_client.h>
+# endif
 
+#endif
 
 #include "powernet-mib.h"
 #include "rfc1628-mib.h"
@@ -62,13 +55,13 @@
 /*********************************************************************/
 
 struct snmp_ups_internal_data {
-    struct snmp_session session;            /* snmp session struct */
-    char device[MAXSTRING];                 /* Copy of ups->device */
-    char *peername;                         /* hostname|IP of peer */
-    unsigned short remote_port;             /* Remote socket, usually 161 */
-    char *DeviceVendor;                     /* Vendor (ex. APC|RFC) */
-    char *community;                        /* Community name */
-    void *MIB;                              /* Pointer to MIB data */
+   struct snmp_session session;    /* snmp session struct */
+   char device[MAXSTRING];         /* Copy of ups->device */
+   char *peername;                 /* hostname|IP of peer */
+   unsigned short remote_port;     /* Remote socket, usually 161 */
+   char *DeviceVendor;             /* Vendor (ex. APC|RFC) */
+   char *community;                /* Community name */
+   void *MIB;                      /* Pointer to MIB data */
 };
 
 /*********************************************************************/
@@ -86,4 +79,4 @@ extern int snmp_ups_setup(UPSINFO *ups);
 extern int snmp_ups_program_eeprom(UPSINFO *ups, int command, char *data);
 extern int snmp_ups_entry_point(UPSINFO *ups, int command, void *data);
 
-#endif /* _SNMP_H */
+#endif   /* _SNMP_H */
