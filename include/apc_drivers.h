80f184fe3840a0e6a00ea04ed494fcd2c9f85467
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2005-05-31 00:33:05 +0000

    Code style normalization.

--- include/apc_drivers.h
+++ include/apc_drivers.h
@@ -1,60 +1,26 @@
 /*
- *  apc_drivers.h -- header file for exporting UPS drivers.
+ * apc_drivers.h
  *
- *  Copyright (C) 1999-2001 Riccardo Facchetti <riccardo@master.oasi.gpa.it>
- *  
- *  apcupsd.c -- Simple Daemon to catch power failure signals from a
- *               BackUPS, BackUPS Pro, or SmartUPS (from APCC).
- *            -- Now SmartMode support for SmartUPS and BackUPS Pro.
- *
- *  Copyright (C) 1996-99 Andre M. Hedrick <andre@suse.com>
- *  All rights reserved.
+ * Header file for exporting UPS drivers.
  */
 
 /*
- *                     GNU GENERAL PUBLIC LICENSE
- *                        Version 2, June 1991
+ * Copyright (C) 1999-2001 Riccardo Facchetti <riccardo@master.oasi.gpa.it>
+ * Copyright (C) 1996-1999 Andre M. Hedrick <andre@suse.com>
  *
- *  Copyright (C) 1989, 1991 Free Software Foundation, Inc.
- *                           675 Mass Ave, Cambridge, MA 02139, USA
- *  Everyone is permitted to copy and distribute verbatim copies
- *  of this license document, but changing it is not allowed.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General
+ * Public License as published by the Free Software Foundation.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-
-/*
- *  IN NO EVENT SHALL ANY AND ALL PERSONS INVOLVED IN THE DEVELOPMENT OF THIS
- *  PACKAGE, NOW REFERRED TO AS APCUPSD-Team BE LIABLE TO ANY PARTY FOR
- *  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING
- *  OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF ANY OR ALL
- *  OF THE APCUPSD-Team HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *  THE APCUPSD-Team SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
- *  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- *  FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
- *  ON AN AS IS BASIS, AND THE APCUPSD-Team HAS NO OBLIGATION TO PROVIDE
- *  MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
- *
- *  THE APCUPSD-Team HAS ABSOLUTELY NO CONNECTION WITH THE COMPANY
- *  AMERICAN POWER CONVERSION, APCC.  THE APCUPSD-Team DID NOT AND
- *  HAS NOT SIGNED ANY NON-DISCLOSURE AGREEMENTS WITH APCC.  ANY AND ALL
- *  OF THE LOOK-A-LIKE ( UPSlink(tm) Language ) WAS DERIVED FROM THE
- *  SOURCES LISTED BELOW.
- *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
  */
 
 #ifndef _APC_DRIVERS_H
@@ -121,29 +87,24 @@
  */
 
 typedef struct upsdriver {
-    /*
-     * Data side of the driver structure.
-     */
-    const char *driver_name;
+   /* Data side of the driver structure. */
+   const char *driver_name;
 
-    /*
-     * Functions side of the driver structure.
-     */
-    int (*open)(UPSINFO *ups);
-    int (*setup)(UPSINFO *ups);
-    int (*close)(UPSINFO *ups);
-    int (*kill_power)(UPSINFO *ups);
-    int (*read_ups_static_data)(UPSINFO *ups);
-    int (*read_ups_volatile_data)(UPSINFO *ups);
-    int (*get_ups_capabilities)(UPSINFO *ups);
-    int (*check_ups_state)(UPSINFO *ups);
-    int (*ups_program_eeprom)(UPSINFO *ups, int command, char *data);
-    int (*ups_entry_point)(UPSINFO *ups, int command, void *data);
+   /* Functions side of the driver structure. */
+   int (*open) (UPSINFO *ups);
+   int (*setup) (UPSINFO *ups);
+   int (*close) (UPSINFO *ups);
+   int (*kill_power) (UPSINFO *ups);
+   int (*read_ups_static_data) (UPSINFO *ups);
+   int (*read_ups_volatile_data) (UPSINFO *ups);
+   int (*get_ups_capabilities) (UPSINFO *ups);
+   int (*check_ups_state) (UPSINFO *ups);
+   int (*ups_program_eeprom) (UPSINFO *ups, int command, char *data);
+   int (*ups_entry_point) (UPSINFO *ups, int command, void *data);
 } UPSDRIVER;
 
-/*
- * Some defines that helps code readability.
- */
+/* Some defines that helps code readability. */
+/* FIXME (adk): Danger...dangling else clauses */
 #define device_open(ups) \
     if (ups->driver) ups->driver->open(ups)
 #define device_setup(ups) \
@@ -165,24 +126,18 @@ typedef struct upsdriver {
 #define device_entry_point(ups, command, data) \
     if (ups->driver) ups->driver->ups_entry_point(ups, command, data)
 
-/*
- * Now some defines for device_entry_point commands.
- */
-/*
- * Dumb entry points.
- */
+/* Now some defines for device_entry_point commands. */
+
+/* Dumb entry points. */
 #define DEVICE_CMD_DTR_ENABLE       0x00
 #define DEVICE_CMD_DTR_ST_DISABLE   0x01
-/*
- * Smart entry points.
- */
+
+/* Smart entry points. */
 #define DEVICE_CMD_GET_SELFTEST_MSG 0x02
 #define DEVICE_CMD_CHECK_SELFTEST   0x03
 #define DEVICE_CMD_SET_DUMB_MODE    0x04
 
-/*
- * Support routines.
- */
+/* Support routines. */
 const UPSDRIVER *attach_driver(UPSINFO *ups);
 
 #endif /*_APC_DRIVERS_H */
