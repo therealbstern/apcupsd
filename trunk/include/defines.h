56012399cc392904aa422e251bbc77afebbbfe26
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2005-09-24 15:58:21 +0000

    Fix TV_DIFF_MS macro.

--- trunk/include/defines.h
+++ trunk/include/defines.h
@@ -516,7 +516,7 @@ void d_msg(const char *file, int line, int level, const char *fmt, ...);
 
 /* Determine the difference, in milliseconds, between two struct timevals. */
 #define TV_DIFF_MS(a, b) \
-    ((b).tv_sec - (a).tv_sec) * 1000 + ((b).tv_usec - (a).tv_usec) / 1000;
+    (((b).tv_sec - (a).tv_sec) * 1000 + ((b).tv_usec - (a).tv_usec) / 1000)
 
 
 #endif   /* _DEFINES_H */
