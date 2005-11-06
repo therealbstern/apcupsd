3e399a5899d9fb9b84fbabfc48437606a5c3e99a
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2005-11-06 22:49:59 +0000

    Add support for SNMP traps in powernet_snmp_ups_check_state().

--- trunk/src/drivers/snmp/drv_powernet.c
+++ trunk/src/drivers/snmp/drv_powernet.c
@@ -116,7 +116,7 @@ int powernet_snmp_kill_ups_power(UPSINFO *ups)
    }
 
    if (response->errstat != SNMP_ERR_NOERROR) {
-      Dmsg0(0, "Unable to kill UPS power: can not set SNMP variable.\n");
+      Dmsg1(0, "Unable to kill UPS power: can not set SNMP variable (%d).\n", response->errstat);
       return 0;
    }
 
@@ -462,87 +462,128 @@ int powernet_snmp_ups_read_volatile_data(UPSINFO *ups)
    return 1;
 }
 
+/* Callback invoked by SNMP library when an async event arrives */
+static int powernet_snmp_callback(
+   int operation, netsnmp_session *session, 
+   int reqid, netsnmp_pdu *pdu, void *magic)
+{
+   struct snmp_ups_internal_data *my_data =
+      (struct snmp_ups_internal_data *)magic;
+
+   Dmsg1(100, "powernet_snmp_callback: %d\n", reqid);
+
+   if (reqid == 0)
+      my_data->trap_received = true;
+
+   return NETSNMP_CALLBACK_OP_RECEIVED_MESSAGE;
+}
+
 int powernet_snmp_ups_check_state(UPSINFO *ups)
 {
    struct snmp_ups_internal_data *Sid =
       (struct snmp_ups_internal_data *)ups->driver_internal_data;
-   struct snmp_session *s = &Sid->session;
-   powernet_mib_t *data = (powernet_mib_t *)Sid->MIB;
+   fd_set fds;
+   int numfds, rc, block;
+   struct timeval tmo, exit, now;
 
-   if (powernet_check_comm_lost(ups) == 0)
+   /* Check for commlost under lock since UPS status might be changed */
+   write_lock(ups);
+   rc = powernet_check_comm_lost(ups);
+   write_unlock(ups);
+   if (rc == 0)
       return 0;
 
-   powernet_mib_mgr_get_upsBasicBattery(s, &(data->upsBasicBattery));
-   if (data->upsBasicBattery) {
-      switch (data->upsBasicBattery->__upsBasicBatteryStatus) {
-      case 2:
-         ups->clear_battlow();
+   /* If we're not doing SNMP traps, just sleep and exit */
+   if (!Sid->trap_session) {
+      sleep(ups->wait_time);
+      return 1;
+   }
+
+   /* Figure out when we need to exit by */
+   gettimeofday(&exit, NULL);
+   exit.tv_sec += ups->wait_time;
+
+   while(1)
+   {
+      /* Figure out how long until we have to exit */
+      gettimeofday(&now, NULL);
+
+      if (now.tv_sec > exit.tv_sec ||
+         (now.tv_sec == exit.tv_sec &&
+            now.tv_usec >= exit.tv_usec)) {
+         /* Done already? How time flies... */
+         return 0;
+      }
+
+      tmo.tv_sec = exit.tv_sec - now.tv_sec;
+      tmo.tv_usec = exit.tv_usec - now.tv_usec;
+      if (tmo.tv_usec < 0) {
+         tmo.tv_sec--;              /* Normalize */
+         tmo.tv_usec += 1000000;
+      }
+
+      /* Get select parameters from SNMP library */
+      FD_ZERO(&fds);
+      block = 0;
+      snmp_select_info(&numfds, &fds, &tmo, &block);
+
+      /* Wait for something to happen */
+      rc = select(numfds, &fds, NULL, NULL, &tmo);
+      switch (rc) {
+      case 0:  /* Timeout */
+         /* Tell SNMP library about the timeout */
+         snmp_timeout();
          break;
-      case 3:
-         ups->set_battlow();
-         break;
-      default:                    /* Unknown, assume battery is ok */
-         ups->clear_battlow();
+
+      case -1: /* Error */
+         if (errno == EINTR || errno == EAGAIN)
+            continue;            /* assume SIGCHLD */
+
+         Dmsg1(200, "select error: ERR=%s\n", strerror(errno));
+         return 0;
+
+      default: /* Data available */
+         /* Reset trap flag and run callback processing */
+         Sid->trap_received = false;
+         snmp_read(&fds);
+
+         /* If callback processing set the flag, we got a trap */
+         if (Sid->trap_received)
+            return 1;
+
          break;
       }
-      free(data->upsBasicBattery);
+   }
+}
+
+int powernet_snmp_ups_open(UPSINFO *ups)
+{
+   struct snmp_ups_internal_data *my_data =
+      (struct snmp_ups_internal_data *)ups->driver_internal_data;
+   struct snmp_session *s = &my_data->session;
+   struct snmp_session tmp;
+
+   /* If we're configured to not use traps, just exit */
+   if (!strcmp(my_data->DeviceVendor, "APC_NOTRAP")) {
+      Dmsg0(100, "User requested no traps\n");
+      return 1;
    }
 
-   powernet_mib_mgr_get_upsAdvBattery(s, &(data->upsAdvBattery));
-   if (data->upsAdvBattery) {
-      if (data->upsAdvBattery->__upsAdvBatteryReplaceIndicator == 2)
-         ups->set_replacebatt();
-      else
-         ups->clear_replacebatt();
+   /* Trap session is a copy of client session with some tweaks */
+   tmp = *s;
+   tmp.peername = "0.0.0.0:162";   /* Listen to snmptrap port on all interfaces */
+   tmp.local_port = 1;             /* We're a server, not a client */
+   tmp.callback = powernet_snmp_callback;
+   tmp.callback_magic = my_data;
 
-      free(data->upsAdvBattery);
-   }
-
-   powernet_mib_mgr_get_upsBasicOutput(s, &(data->upsBasicOutput));
-   if (data->upsBasicOutput) {
-      /* Clear the following flags: only one status will be TRUE */
-      Dmsg1(99, "Status before clearing: 0x%08x\n", ups->Status);
-      ups->clear_online();
-      ups->clear_onbatt();
-      ups->clear_boost();
-      ups->clear_trim();
-      Dmsg1(99, "Status after clearing: 0x%08x\n", ups->Status);
-
-      switch (data->upsBasicOutput->__upsBasicOutputStatus) {
-      case 2:
-         ups->set_online();
-         break;
-      case 3:
-         ups->set_onbatt();
-         break;
-      case 4:
-         ups->set_boost();
-         break;
-      case 12:
-         ups->set_trim();
-         break;
-      case 1:                     /* unknown */
-      case 5:                     /* timed sleeping */
-      case 6:                     /* software bypass */
-      case 7:                     /* UPS off */
-      case 8:                     /* UPS rebooting */
-      case 9:                     /* switched bypass */
-      case 10:                    /* hardware failure bypass */
-      case 11:                    /* sleeping until power returns */
-      default:                    /* unknown */
-         break;
-      }
-      free(data->upsBasicOutput);
-   }
-
-   powernet_mib_mgr_get_upsAdvTest(s, &(data->upsAdvTest));
-   if (data->upsAdvTest) {
-      if (data->upsAdvTest->__upsAdvTestCalibrationResults == 3)
-         ups->set_calibration();
-      else
-         ups->clear_calibration();
-
-      free(data->upsAdvTest);
+   /*
+    * Open the trap session and store it in my_data.
+    * It's ok if this fails; the code will fall back to polling.
+    */
+   my_data->trap_session = snmp_open(&tmp);
+   if (!my_data->trap_session) {
+      Dmsg0(100, "Trap session failed to open\n");
+      return 1;
    }
 
    return 1;
