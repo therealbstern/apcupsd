814f54a618765487f72bb898938dd4246a68bde4
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2005-12-02 03:19:38 +0000

    Fix compile warning. Remove unused code.

--- src/drivers/usb/generic/hidutils.c
+++ src/drivers/usb/generic/hidutils.c
@@ -39,6 +39,7 @@
 
 #define MAX_SANE_DESCRIPTOR_LEN 4096
 
+/* Fetch a descriptor from an interface (as opposed to from the device) */
 int usb_get_intf_descriptor(usb_dev_handle *udev, unsigned char type,
                             unsigned char index, void *buf, int size)
 {
@@ -57,8 +58,6 @@ int usb_get_intf_descriptor(usb_dev_handle *udev, unsigned char type,
  */
 unsigned char *hidu_fetch_report_descriptor(usb_dev_handle *fd, int *rlen)
 {
-#if 1
-
    unsigned char *ptr;
    int rdesclen, i;
 
@@ -82,127 +81,6 @@ unsigned char *hidu_fetch_report_descriptor(usb_dev_handle *fd, int *rlen)
 
    *rlen = rdesclen;
    return ptr;
-
-#else
-
-   int rc, i, rdesclen;
-   struct usb_config_desc cdesc;
-   struct usb_full_desc fdesc;
-   struct usb_ctl_request req;
-   int len;
-   unsigned char *ptr;
-
-   /*
-    * In order to fetch the report descriptor we need to first
-    * determine that descriptor's length. Unlike the other std
-    * descriptors, report descriptors are not prefixed with their
-    * length. We must instead look in the HID descriptor. This is
-    * made especially painful because for some reason we cannot
-    * request the HID descriptor directly. The length bytes come
-    * back munged when we do that (bad, FreeBSD, bad!). So instead
-    * we ask for the set of all top level descriptors and dig the
-    * HID descriptor out of there. Then we can *finally* go about
-    * asking for the report descriptor itself.
-    */
-
-   /*
-    * First, get the CONFIG descriptor alone so we can look up
-    * the length of the full descriptor set.
-    */
-   cdesc.ucd_config_index = USB_CURRENT_CONFIG_INDEX;
-   rc = ioctl(fd, USB_GET_CONFIG_DESC, &cdesc);
-   if (rc) {
-      Dmsg0(100, "Unable to get USB CONFIG descriptor.\n");
-      return NULL;
-   }
-
-   len = UGETW(cdesc.ucd_desc.wTotalLength);
-   if (!len || len > MAX_SANE_DESCRIPTOR_LEN) {
-      Dmsg1(100, "Unreasonable length %d.\n", len);
-      return NULL;
-   }
-
-   /*
-    * Now get the full set of descriptors (does not include
-    * report descriptor).
-    */
-   fdesc.ufd_size = len;
-   fdesc.ufd_data = (u_char*)malloc(len);
-   fdesc.ufd_config_index = USB_CURRENT_CONFIG_INDEX;
-   rc = ioctl(fd, USB_GET_FULL_DESC, &fdesc);
-   if (rc) {
-      Dmsg0(100, "Unable to get full descriptors.\n");
-      free(fdesc.ufd_data);
-      return NULL;
-   }
-
-   if (debug_level >= 300) {
-      printf("Full descriptors (length=%d):\n", len);
-      for (i = 0; i < len; i++)
-         printf("%02x, ", fdesc.ufd_data[i]);
-      printf("\n");
-   }
-
-   /* Search for the HID descriptor */
-   for (ptr = fdesc.ufd_data, i = 0; i < len; i += ptr[0], ptr += ptr[0])
-      if (ptr[1] == UDESC_HID)
-         break;
-
-   if (i >= len) {
-      Dmsg0(100, "Unable to locate HID descriptor.\n");
-      free(fdesc.ufd_data);
-      return NULL;
-   }
-
-   /* We expect the first additional descriptor type to be report */
-   if (ptr[6] != UDESC_REPORT) {
-      Dmsg0(100, "First extra descriptor not report.\n");
-      free(fdesc.ufd_data);
-      return NULL;
-   }
-
-   /* Finally! The report descriptor's length! */
-   rdesclen = ptr[8] << 8 | ptr[7];
-   Dmsg2(200, "Report desc len=0x%04x (%d)\n", rdesclen, rdesclen);
-
-   /* That's all we needed from the buffer */
-   free(fdesc.ufd_data);
-
-   if (!rdesclen || rdesclen > MAX_SANE_DESCRIPTOR_LEN) {
-      Dmsg1(100, "Unreasonable rdesclen %d.\n", rdesclen);
-      return NULL;
-   }
-
-   /*
-    * Now fetch the report descriptor itself. We use a raw USB request
-    * for this because the report descriptor is a class specific item.
-    */
-   req.ucr_flags = 0;
-   req.ucr_actlen = 0;
-   req.ucr_addr = 0;
-   req.ucr_data = malloc(rdesclen);
-   req.ucr_request.bmRequestType = UT_READ_INTERFACE;
-   req.ucr_request.bRequest = UR_GET_DESCRIPTOR;
-   USETW(req.ucr_request.wValue, UDESC_REPORT << 8);
-   USETW(req.ucr_request.wIndex, 0);
-   USETW(req.ucr_request.wLength, rdesclen);
-   rc = ioctl(fd, USB_DO_REQUEST, &req);
-   if (rc) {
-      Dmsg0(100, "Unable to read report descriptor.\n");
-      free(req.ucr_data);
-      return NULL;
-   }
-
-   if (debug_level >= 300) {
-      printf("Report descriptor (length=%d):\n", rdesclen);
-      for (i = 0; i < rdesclen; i++)
-         printf("%02x, ", ((unsigned char *)req.ucr_data)[i]);
-      printf("\n");
-   }
-
-   *rlen = rdesclen;
-   return (unsigned char *)req.ucr_data;
-#endif
 }
 
 /* Push a value onto the collection stack */
@@ -286,7 +164,7 @@ int hidu_locate_item(report_desc_t rdesc, int usage, int app, int phys,
             PUSH_COLLECTION(app, item.usage);
       }
 
-      if (usage != -1 && usage != item.usage)
+      if (usage != -1 && (unsigned int)usage != item.usage)
          goto next;
       if (app != -1 && app != TOP_COLLECTION(app))
          goto next;
