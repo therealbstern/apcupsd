bead5fc49313c383605e1fd16eb2c7071e340f7f
kerns <kerns@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2006-05-01 19:16:23 +0000

    Update README

--- trunk/src/apcnis.c
+++ trunk/src/apcnis.c
@@ -5,7 +5,7 @@
  */
 
 /*
- * Copyright (C) 1999-2005 Kern Sibbald
+ * Copyright (C) 1999-2006 Kern Sibbald
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General
