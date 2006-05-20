28bc933450bef882fc0594bdb4bd9d7f79a949ab
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2006-05-20 05:03:11 +0000

    Build shutdown.exe and popup.exe on Mingw32.

--- trunk/src/win32/shutdown.c
+++ trunk/src/win32/shutdown.c
@@ -10,9 +10,10 @@
 #include   <stdio.h>
 #include   <stdlib.h>
 
+#ifndef HAVE_MINGW
 extern void mainCRTStartup();
-
 void WinMainCRTStartup() { mainCRTStartup(); }
+#endif
 
 int main(int argc, char **argv)
 {
