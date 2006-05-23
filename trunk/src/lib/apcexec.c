23b615cb2940c7f3dd6cee7ad2e3659be51a60ce
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2006-05-23 21:55:07 +0000

    Add a first-cut implementation of execute_command() for Mingw.

--- trunk/src/lib/apcexec.c
+++ trunk/src/lib/apcexec.c
@@ -74,6 +74,71 @@ void clean_threads(void)
    }
 }
 
+#ifdef HAVE_MINGW
+int execute_command(UPSINFO *ups, UPSCOMMANDS cmd)
+{
+   char cmdline[MAXSTRING];
+   char *comspec;
+   PROCESS_INFORMATION procinfo;
+   STARTUPINFOA startinfo;
+   BOOL rc;
+   char apccontrol[strlen(APCCONTROL)+1];
+
+   if (cmd.pid && (kill(cmd.pid, 0) == 0)) {
+      /*
+       * Command is already running. No point in running it two
+       * times.
+       */
+      return SUCCESS;
+   }
+
+   /* Find command interpreter */
+   comspec = getenv("COMSPEC");
+   if (comspec == NULL)
+      return FAILURE;
+
+   /* HACK! The APCCONTROL constant is currently using UNIX slashes */
+   conv_unix_to_win32_path(APCCONTROL, apccontrol, sizeof(apccontrol));
+
+   /* Build the command line */
+   asnprintf(cmdline, sizeof(cmdline), "/c %s %s \"%s\" %d %d",
+      apccontrol, cmd.command, ups->upsname,
+      !ups->is_slave(), ups->is_plugged());
+
+   /* Initialize the STARTUPINFOA struct; no special settings needed. */
+   memset(&startinfo, 0, sizeof(startinfo));
+   startinfo.cb = sizeof(startinfo);
+
+   Dmsg2(200, "execute_command: CreateProcessA(%s, %s, ...)\n",
+      comspec, cmdline);
+
+   /* Execute the process */
+   rc = CreateProcessA(comspec,
+                       cmdline, // command line
+                       NULL, // process security attributes
+                       NULL, // primary thread security attributes
+                       TRUE, // handles are inherited
+                       0,    // creation flags
+                       NULL, // use parent's environment
+                       NULL, // use parent's current directory
+                       &startinfo, // STARTUPINFO pointer
+                       &procinfo); // receives PROCESS_INFORMATION
+   if (!rc) {
+      log_event(ups, LOG_WARNING, "execute failed: CreateProcessA(%s, %s, ...)=%d\n",
+         comspec, cmdline, GetLastError());
+      return FAILURE;
+   }
+
+   /* Extract pid */
+   cmd.pid = procinfo.dwProcessId;
+
+   /* Don't need handles */
+   CloseHandle(procinfo.hProcess);
+   CloseHandle(procinfo.hThread);
+
+   return SUCCESS;
+}
+#else
 int execute_command(UPSINFO *ups, UPSCOMMANDS cmd)
 {
    char *argv[6];
@@ -137,3 +202,4 @@ int execute_command(UPSINFO *ups, UPSCOMMANDS cmd)
 
    return SUCCESS;
 }
+#endif /* HAVE_MINGW */
