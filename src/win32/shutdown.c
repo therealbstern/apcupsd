e9aa5acdaf7db3160f3eeea2df3747bb07d5c0df
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2006-07-15 17:17:49 +0000

    Simplify code based on testing all modes on Win98 and WinXP.

--- src/win32/shutdown.c
+++ src/win32/shutdown.c
@@ -6,139 +6,119 @@
  *   Kern E. Sibbald, July MM
  */
 
-#include   <windows.h>
-#include   <stdio.h>
-#include   <stdlib.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
 #ifndef HAVE_MINGW
 extern void mainCRTStartup();
 void WinMainCRTStartup() { mainCRTStartup(); }
 #endif
 
+enum {
+   MODE_CANCEL,
+   MODE_HALT,
+   MODE_REBOOT
+};
+
 int main(int argc, char **argv)
 {
-   char *usage;
-   DWORD   g_platform_id;
-   DWORD   Timeout = 30;
-   OSVERSIONINFO osversioninfo;
-   osversioninfo.dwOSVersionInfoSize = sizeof(osversioninfo);
+   char ch;
+   int mode = MODE_HALT;
+   int timeout = 0;
+   int force = 1;
+   char* message;
+
+   // Process command line args
+   while ((ch = getopt(argc, argv, "+chrf")) != -1) {
+      switch (ch) {
+      case 'c':
+         mode = MODE_CANCEL;
+         break;
+      case 'h':
+         mode = MODE_HALT;
+         break;
+      case 'r':
+         mode = MODE_REBOOT;
+         break;
+      case 'f':
+         force = 0;
+         break;
+      }
+   }
+
+   if (optind < argc) {
+      if (stricmp(argv[optind], "now") == 0)
+         timeout = 0;
+      else
+         timeout = strtoul(argv[optind], NULL, 0);
+      optind++;
+   }
+
+   if (optind < argc) {
+      message = argv[optind];
+   } else {
+      message = "Power failure system going down!";
+   }
 
    // Get the current OS version
-   if (!GetVersionEx(&osversioninfo))
-      g_platform_id = 0;
-   else
-      g_platform_id = osversioninfo.dwPlatformId;
-       
-   /* For WinNT and Win2000, we must get permission */
-   if (g_platform_id == VER_PLATFORM_WIN32_NT) { 
+   OSVERSIONINFO ver;
+   ver.dwOSVersionInfoSize = sizeof(ver);
+   GetVersionEx(&ver);
+
+   /* For WinNT and above, we must get permission */
+   if (ver.dwPlatformId == VER_PLATFORM_WIN32_NT) { 
 
       HANDLE hToken; 
       TOKEN_PRIVILEGES tkp; 
-       
+
       // Get a token for this process. 
 #ifdef HAVE_CYGWIN
-      if (!OpenThreadToken(GetCurrentThread(), 
-         TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, FALSE, &hToken)) {
+      OpenThreadToken(GetCurrentThread(), 
+         TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, FALSE, &hToken);
 #else
-      if (!OpenProcessToken(GetCurrentProcess(), 
-         TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
+      OpenProcessToken(GetCurrentProcess(), 
+         TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
 #endif
-// Forge onward anyway in the hopes of succeeding.
-//       MessageBox(NULL, "System shutdown failed: OpenProcessToken", "shutdown", MB_OK);
-//	 exit(1);
-      } 
 
       // Get the LUID for the shutdown privilege. 
-       
+
       LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, 
 	      &tkp.Privileges[0].Luid); 
-       
+
       tkp.PrivilegeCount = 1;  // one privilege to set	  
       tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
-       
+
       // Get the shutdown privilege for this process. 
-       
+
       AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, 
-	      (PTOKEN_PRIVILEGES)NULL, 0); 
-       
-      // Cannot test the return value of AdjustTokenPrivileges. 
-       
-      if (GetLastError() != ERROR_SUCCESS) {
-//       MessageBox(NULL, "System shutdown failed: AdjustTokePrivileges", "shutdown", MB_OK);
-//	 exit(1);
-      } 
+	      (PTOKEN_PRIVILEGES)NULL, 0);
    }
 
-   /* Now, do the real work */
-   if (argc >= 2 && (strcmp(argv[1], "-c") == 0)) {   /* Cancel shutdown */
-      if (!AbortSystemShutdown(
-	      NULL			  /* system name */
-	     )) {
-         MessageBox(NULL, "Cancel Shutdown request failed.", "shutdown", MB_OK);
-	 exit(1);
-      } 
-      exit(0);
-   }
-
-   if (argc == 2 && (strcmp(argv[1], "-r") == 0)) {   /* Reboot */
-      /* Try gentle way */
-      ExitWindowsEx(EWX_REBOOT, 0);
-      Sleep(Timeout*1000);
-      /* Force it */
-      if (!ExitWindowsEx(EWX_REBOOT|EWX_FORCE, 0)) {
-         MessageBox(NULL, "System reboot request failed.", "shutdown", MB_OK);
-	 exit(1);
-      } 
-      exit(0);
-   }
-
-   if (argc != 3 || strcmp(argv[1], "-h")) {
-      usage = "Incorrect command line arguments given to Shutdown\n\n\
-Usage: shutdown [-chr] <now>\n\
-       -c:      cancel shutdown.\n\
-       -h:      halt after shutdown. (requires now)\n\
-       -r:      reboot after shutdown.\n\n";
-      printf(usage);
-      MessageBox(NULL, usage, "Shutdown", MB_OK);
-      exit(1);
-   }
-
-   if (strcmp(argv[2], "now") == 0) {
-      Timeout = 30;		      /* give 30 seconds anyway */
+   if (mode == MODE_CANCEL) {
+      AbortSystemShutdown(NULL);
    } else {
-      Timeout = atoi(argv[2]);
-      if (Timeout < 0) {
-	 Timeout = 0;
+      if (ver.dwPlatformId == VER_PLATFORM_WIN32_NT) {
+         InitiateSystemShutdown(
+            NULL,                                  /* Local machine */
+            message,                               /* Message */
+            timeout,                               /* Timeout (secs) */
+            force,                                 /* Force */
+            mode == MODE_REBOOT);                  /* Reboot */
+      } else {
+         Sleep(timeout*1000);
+
+         int action = (mode == MODE_REBOOT) ? EWX_REBOOT : EWX_SHUTDOWN;
+
+         if (force)
+            action |= EWX_FORCE;
+
+         ExitWindowsEx(
+            action, /* Action */
+            0);     /* Reason */
       }
    }
 
-   /* For WinNT and Win2000, we must do it differently */
-   if (g_platform_id == VER_PLATFORM_WIN32_NT) { 
-      /* Try gentle way */
-      ExitWindowsEx(EWX_SHUTDOWN, 0);
-      /* Now force everything after 30 second warning */
-      if (!InitiateSystemShutdown(
-	 NULL,			      /* system name */
-         "Power failure system going down!", /* message */
-	 Timeout,		      /* Time */
-	 1,			      /* Force close option */
-	 0			      /* reboot option */
-	 )) {
-         MessageBox(NULL, "System shutdown request failed.", "shutdown", MB_OK);
-	 exit(1);
-      }
-
-   } else {		    /* Win98/95 */
-      /* Try gentle way */
-      ExitWindowsEx(EWX_SHUTDOWN, 0);
-
-      Sleep(Timeout*1000);
-
-      /* Force it */
-      if (!ExitWindowsEx(EWX_SHUTDOWN|EWX_FORCE, 0)) {
-         MessageBox(NULL, "System shutdown request failed.", "shutdown", MB_OK);
-	 exit(1);
-      }
-   }
    exit(0);
 }
