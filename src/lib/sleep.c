852c92e32368689b41232b42744a3ddc9139b8e7
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2006-07-24 23:22:56 +0000

    Add boilerplate comments to new files.

--- src/lib/sleep.c
+++ src/lib/sleep.c
@@ -1,3 +1,26 @@
+/*
+ * sleep.c
+ *
+ * Implementations of sleep-related functions for platforms that
+ * do not already have them.
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
 #include "apc.h"
 
 #ifndef HAVE_NANOSLEEP
