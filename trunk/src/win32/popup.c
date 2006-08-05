b5113d64295790246f2f39d1bf36802b4946f8d9
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2006-08-05 19:37:17 +0000

    Use balloon notification on Apcupsd tray icon when possible. Fall back on previous behavior (MessageBox) when necessary.

--- trunk/src/win32/popup.c
+++ trunk/src/win32/popup.c
@@ -7,7 +7,9 @@
  *   July MM  
  */
 #include "windows.h"
-
+#include "winres.h"
+#include "winups.h"
+#include <stdio.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
 		   PSTR szCmdLine, int iCmdShow)
@@ -42,6 +44,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
       msg = szCmdLine + 1;
       szCmdLine[len-1] = 0;
    }
-   MessageBox(NULL, msg, "Apcupsd message", MB_OK);
+
+   // Locate Apcupsd tray icon window
+   HWND tray = FindWindowEx(NULL, NULL, "Apcupsd Tray Icon", NULL);
+   
+   // Win2k and higher use balloon notification,
+   // others have to use MessageBox
+   OSVERSIONINFO vers;
+   vers.dwOSVersionInfoSize = sizeof(vers);
+   if (tray == NULL || GetVersionEx(&vers) == 0 || vers.dwMajorVersion < 5) {
+      MessageBox(NULL, msg, "Apcupsd message", MB_OK);
+   } else {
+      NOTIFYICONDATA nid;
+
+      // Create the tray icon message
+      nid.hWnd = tray;
+      nid.cbSize = sizeof(nid);
+      nid.uID = IDI_APCUPSD;        // never changes after construction
+
+      // Setup balloon tip with detailed status info
+      snprintf(nid.szInfo, sizeof(nid.szInfo), "%s", msg);
+      snprintf(nid.szInfoTitle, sizeof(nid.szInfoTitle), "%s", "Apcupsd Notice");
+      nid.uFlags = NIF_INFO;
+      nid.uTimeout = 5000;
+      nid.dwInfoFlags = NIIF_INFO;
+
+      // Send the message
+      Shell_NotifyIcon(NIM_MODIFY, &nid);
+
+      // Notify apcupsd tray that a balloon was shown and
+      // ask for a 5000 msec timeout;
+      SendNotifyMessage(tray, WM_BALLOONSHOW, 10000, 0);
+   }
+
    return 0;
 }
