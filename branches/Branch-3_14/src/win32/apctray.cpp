b3b7835c8b8bf54bc0f6fa5dda8aa64b37fea297
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2007-06-02 21:34:04 +0000

    Add support for setting host, port, and refresh interval via command line.

--- branches/Branch-3_14/src/win32/apctray.cpp
+++ branches/Branch-3_14/src/win32/apctray.cpp
@@ -12,6 +12,51 @@
 #include "winups.h"
 #include "statmgr.h"
 
+
+#define CMDOPT_PORT     "/port"
+#define CMDOPT_HOST     "/host"
+#define CMDOPT_INTERVAL "/interval"
+
+#define USAGE_TEXT   "[/host <hostname>] [/port <port>] [/interval <sec>]"
+
+char *GetArg(char **cmdline)
+{
+   // Skip leading whitespace
+   while (isspace(**cmdline))
+      (*cmdline)++;
+
+   // Bail if there's nothing left
+   if (**cmdline == '\0')
+      return NULL;
+
+   // Find end of this argument
+   char *ret;
+   if (**cmdline == '"') {
+      // Find end of quoted argument
+      ret = ++(*cmdline);
+      while (**cmdline && **cmdline != '"')
+         (*cmdline)++;
+   } else {
+      // Find end of non-quoted argument
+      ret = *cmdline;
+      while (**cmdline && !isspace(**cmdline))
+         (*cmdline)++;
+   }
+
+   // NUL-terminate this argument
+   if (**cmdline)
+      *(*cmdline)++ = '\0';
+
+   return ret;
+}
+
+void Usage(const char *text1, const char* text2)
+{
+   MessageBox(NULL, text1, text2, MB_OK);
+   MessageBox(NULL, USAGE_TEXT, "Apctray Usage",
+              MB_OK | MB_ICONINFORMATION);
+}
+
 // WinMain parses the command line and either calls the main App
 // routine or, under NT, the main service routine.
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
@@ -20,11 +65,44 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
    InitWinAPIWrapper();
    WSA_Init();
 
+   // Default settings
+   char *host = "127.0.0.1";
+   unsigned short port = 3551;
+   int interval = 1000;
+
+   // Check command line options
+   char *arg;
+   char *opt = CmdLine;
+   while ((arg = GetArg(&opt))) {
+      if (strcasecmp(arg, CMDOPT_HOST) == 0) {
+         if (!(arg = GetArg(&opt))) {
+            Usage(CMDOPT_HOST, "Option requires argument");
+            return 1;
+         }
+         host = arg;
+      } else if (strcasecmp(arg, CMDOPT_PORT) == 0) {
+         if (!(arg = GetArg(&opt))) {
+            Usage(CMDOPT_PORT, "Option requires argument");
+            return 1;
+         }
+         port = strtoul(arg, NULL, 0);
+      } else if (strcasecmp(arg, CMDOPT_INTERVAL) == 0) {
+         if (!(arg = GetArg(&opt))) {
+            Usage(CMDOPT_INTERVAL, "Option requires argument");
+            return 1;
+         }
+         interval = strtoul(arg, NULL, 0) * 1000;
+      } else {
+         Usage(arg, "Unknown option");
+         return 1;
+      }
+   }
+
    // Create a StatMgr for handling UPS status queries
-   StatMgr *statmgr = new StatMgr("pia", 3551);
+   StatMgr *statmgr = new StatMgr(host, port);
 
    // Create tray icon & menu
-   upsMenu *menu = new upsMenu(hInstance, statmgr, 1000);
+   upsMenu *menu = new upsMenu(hInstance, statmgr, interval);
    if (menu == NULL) {
       PostQuitMessage(0);
    }
@@ -36,10 +114,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
       DispatchMessage(&msg);
    }
 
-   if (menu != NULL) {
-      delete menu;
-   }
-
+   delete menu;
    delete statmgr;
    WSACleanup();
    return 0;
