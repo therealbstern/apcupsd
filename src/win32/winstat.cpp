b4efad98c171509c5b7674ba4c041af49a09ffa2
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2007-06-02 00:12:56 +0000

    Cleanup of winabout, winevents, and winstat. Everthing compiles again.

--- src/win32/winstat.cpp
+++ src/win32/winstat.cpp
@@ -1,50 +1,26 @@
-//  Copyright (C) 1999 AT&T Laboratories Cambridge. All Rights Reserved.
-//
-//  This file was part of the VNC system.
-//
-//  The VNC system is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
-//  USA.
-//
-// If the source code for the VNC system is not available from the place 
-// whence you received this file, check http://www.uk.research.att.com/vnc or contact
-// the authors on vnc@uk.research.att.com for information on obtaining it.
-//
 // This file has been adapted to the Win32 version of Apcupsd
 // by Kern E. Sibbald.  Many thanks to ATT and James Weatherall,
 // the original author, for providing an excellent template.
 //
-// Copyright (2000-2003) Kern E. Sibbald
+// Rewrite/Refactoring by Adam Kropelin
 //
-
-
-
-// winStat.cpp
+// Copyright (2007) Adam D. Kropelin
+// Copyright (2000) Kern E. Sibbald
+//
 
 // Implementation of the Status dialog
 
 #include <windows.h>
-
-#include "winups.h"
 #include "winstat.h"
+#include "winres.h"
 
 extern void FillStatusBox(HWND hwnd, int id_list);
 
 // Constructor/destructor
-upsStatus::upsStatus()
+upsStatus::upsStatus(HINSTANCE appinst)
 {
     m_dlgvisible = FALSE;
+    m_appinst = appinst;
 }
 
 upsStatus::~upsStatus()
@@ -52,68 +28,64 @@ upsStatus::~upsStatus()
 }
 
 // Initialisation
-BOOL
-upsStatus::Init()
+BOOL upsStatus::Init()
 {
     return TRUE;
 }
 
 // Dialog box handling functions
-void
-upsStatus::Show(BOOL show)
+void upsStatus::Show(BOOL show)
 {
-    if (show) {
-        if (!m_dlgvisible) {
-            DialogBoxParam(hAppInstance,
+   if (show) {
+      if (!m_dlgvisible) {
+         DialogBoxParam(m_appinst,
                         MAKEINTRESOURCE(IDD_STATUS),
                         NULL,
                         (DLGPROC)DialogProc,
                         (LONG)this);
-        }
-    }
+      }
+   }
 }
 
-BOOL CALLBACK
-upsStatus::DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK upsStatus::DialogProc(
+   HWND hwnd,
+   UINT uMsg,
+   WPARAM wParam,
+   LPARAM lParam)
 {
-    // We use the dialog-box's USERDATA to store a _this pointer
-    // This is set only once WM_INITDIALOG has been recieved, though!
-    upsStatus *_this = (upsStatus *)GetWindowLong(hwnd, GWL_USERDATA);
+   // We use the dialog-box's USERDATA to store a _this pointer
+   // This is set only once WM_INITDIALOG has been recieved, though!
+   upsStatus *_this = (upsStatus *)GetWindowLong(hwnd, GWL_USERDATA);
 
-    switch (uMsg) {
-    case WM_INITDIALOG:
-        // Retrieve the Dialog box parameter and use it as a pointer
-        // to the calling vncProperties object
-        SetWindowLong(hwnd, GWL_USERDATA, lParam);
-        _this = (upsStatus *)lParam;
+   switch (uMsg) {
+   case WM_INITDIALOG:
+      // Retrieve the Dialog box parameter and use it as a pointer
+      // to the calling vncProperties object
+      SetWindowLong(hwnd, GWL_USERDATA, lParam);
+      _this = (upsStatus *)lParam;
 
-        // Show the dialog
-        SetForegroundWindow(hwnd);
-
-        _this->m_dlgvisible = TRUE;
-
-        FillStatusBox(hwnd, IDC_LIST);
-
-        return TRUE;
+      // Show the dialog
+      SetForegroundWindow(hwnd);
+      _this->m_dlgvisible = TRUE;
+      FillStatusBox(hwnd, IDC_LIST);
+      return TRUE;
 
     case WM_COMMAND:
-        switch (LOWORD(wParam)) {
-        case IDCANCEL:
-        case IDOK:
-            // Close the dialog
-            EndDialog(hwnd, TRUE);
+      switch (LOWORD(wParam)) {
+      case IDCANCEL:
+      case IDOK:
+         // Close the dialog
+         EndDialog(hwnd, TRUE);
+         _this->m_dlgvisible = FALSE;
+         return TRUE;
+      }
+      break;
 
-            _this->m_dlgvisible = FALSE;
+   case WM_DESTROY:
+      EndDialog(hwnd, FALSE);
+      _this->m_dlgvisible = FALSE;
+      return TRUE;
+   }
 
-            return TRUE;
-        }
-
-        break;
-
-    case WM_DESTROY:
-        EndDialog(hwnd, FALSE);
-        _this->m_dlgvisible = FALSE;
-        return TRUE;
-    }
-    return 0;
+   return 0;
 }
