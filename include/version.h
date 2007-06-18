0b2ca0ee6bde69a8f2830ef32e6989686fadb807
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2007-06-18 23:38:34 +0000

    Update for 3.14.2-win32beta2 release.

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #define DEBUG 1
-#define VERSION "3.14.2-win32beta1"
-#define ADATE   "10 June 2007"
+#define VERSION "3.14.2-win32beta2"
+#define ADATE   "18 June 2007"
 
 #define APCUPSD_RELEASE VERSION
 
