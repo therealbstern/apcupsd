e5b36a1c0235cff470d8dbc75d2dae34853c8ba5
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2007-10-15 22:12:23 +0000

    Add license text for a* utility classes.

--- trunk/include/amap.h
+++ trunk/include/amap.h
@@ -1,3 +1,30 @@
+/*
+ * amap.h
+ *
+ * Simple map template class build on alist.
+ */
+
+/*
+ * Copyright (C) 2007 Adam Kropelin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef __AMAP_H
+#define __AMAP_H
+ 
 #include "alist.h"
 
 template <class K, class V>
@@ -65,3 +92,5 @@ private:
    // but for now we'll take the easy route and use alist.
    alist<keyval> _map;
 };
+
+#endif // __AMAP_H
