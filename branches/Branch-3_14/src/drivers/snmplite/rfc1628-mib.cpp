eb52ad21c486c04e45957f2880540e7b6789137e
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2010-09-02 19:26:35 +0000

    Minor correction to low battery logic: Depleted also counts as low.

--- branches/Branch-3_14/src/drivers/snmplite/rfc1628-mib.cpp
+++ branches/Branch-3_14/src/drivers/snmplite/rfc1628-mib.cpp
@@ -224,12 +224,18 @@ static void rfc1628_update_ci(UPSINFO *ups, int ci, Snmp::Variable &data)
 
    case CI_LowBattery:
       Dmsg1(80, "Got CI_LowBattery: %d\n", data.u32);
-      if (data.u32 == 3)
+      switch (data.u32)
+      {
+      case 3:  // Low
+      case 4:  // Depleted
          ups->set_battlow();
-      else
+         break;
+      case 1:  // Unknown
+      case 2:  // Normal
+      default:
          ups->clear_battlow();
-      break;
-
+         break;
+      }
    case CI_REVNO:
       Dmsg1(80, "Got CI_REVNO: %s\n", data.str.str());
       astrncpy(ups->firmrev, data.str, sizeof(ups->firmrev));
