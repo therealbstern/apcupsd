6c0e785f96c118d7b6c939b48057abefb69f487f
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2014-03-29 04:26:58 +0000

    Removing bogus tag

--- tags/Release-/include/version.h
+++
@@ -1,9 +0,0 @@
-#define DEBUG 1
-#define VERSION "3.14.12"
-#define ADATE   "29 March 2014"
-
-#define APCUPSD_RELEASE VERSION
-
-#ifndef APCUPSD_HOST
-# define APCUPSD_HOST HOST
-#endif
--- tags/Release-/platforms/debian/examples/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the apcupsd package
-#
-0 4    * * *   root    apcupsd_maintenance
