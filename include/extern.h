85892ddf6411fb82104a6b2ae216bbb6509b4a32
adk0212 <adk0212@53589e1d-ff1b-477b-8bf2-94f7b0080136>
2015-01-20 01:48:49 +0000

    Remove obsolete prototypes

--- include/extern.h
+++ include/extern.h
@@ -51,9 +51,7 @@ extern int dsr_bit;
 
 /* File opened */
 extern int flags;
-
 extern struct termios newtio;
-
 extern int debug_net;
 
 /* getopt flags (see apcoptd.c) */
@@ -69,34 +67,22 @@ extern int shutdown_ups;
 extern int dumb_mode_test;
 extern int go_background;
 
-
-/* In apceeprom.c */
-extern int do_eeprom_programming(UPSINFO *ups);
-
-
 /* In apcopt.c */
 extern int parse_options(int argc, char *argv[]);
 
 /* In apcupsd.c */
-extern void apc_thread_terminate(int sig);
-extern void clean_threads(void);
 extern void apcupsd_terminate(int sig);
 extern void clear_files(void);
-extern void make_pid(void);
 
 /* In apcdevice.c */
 bool setup_device(UPSINFO *ups);
-extern void setup_serial(UPSINFO *ups);
 extern void initiate_hibernate(UPSINFO *ups);
 extern void initiate_shutdown(UPSINFO *ups);
-extern void save_dumb_status(UPSINFO *ups);
-extern int check_serial(UPSINFO *ups);
 extern void prep_device(UPSINFO *ups);
 extern void do_device(UPSINFO *ups);
 extern int fillUPS(UPSINFO *ups);
 
 /* In apcaction.c */
-extern void timer_action(int sig);
 extern void do_action(UPSINFO *ups);
 extern void generate_event(UPSINFO *ups, int event);
 
@@ -104,55 +90,15 @@ extern void generate_event(UPSINFO *ups, int event);
 extern int create_lockfile(UPSINFO *ups);
 extern void delete_lockfile(UPSINFO *ups);
 
-/* In apcnet.c */
-extern int write_struct_net(int wsocketfd, struct netdata *write_struct,
-   size_t size);
-extern int read_struct_net(int rsocketfd, struct netdata *read_struct, size_t size);
-extern void log_struct(struct netdata *logstruct);
-
-extern int reconnect_master(UPSINFO *ups, int who);
-extern int prepare_master(UPSINFO *ups);
-
-extern int reconnect_slave(UPSINFO *ups);
-extern int prepare_slave(UPSINFO *ups);
-extern void kill_net(UPSINFO *ups);
-
-extern void timer_net(int sig);
-extern void do_net(UPSINFO *ups);
-extern void timer_slaves(int sig);
-extern void do_slaves(UPSINFO *ups);
-
 /* In apcfile.c */
 extern int make_file(UPSINFO *ups, const char *path);
 extern void make_pid_file(void);
+extern void make_pid(void);
 
 /* In apcconfig.c */
 extern char APCCONF[APC_FILENAME_MAX];
 extern void init_ups_struct(UPSINFO *ups);
 extern void check_for_config(UPSINFO *ups, char *cfgfile);
-
-/* In apcsetup.c */
-extern void setup_ups_name(UPSINFO *ups);
-extern void setup_ups_replace(UPSINFO *ups);
-
-extern char *setup_ups_string(UPSINFO *ups, char cmd, char *setting);
-extern int setup_ups_single(UPSINFO *ups, char cmd, int single);
-extern int setup_ups_bubble(UPSINFO *ups, char cmd, int setting);
-
-extern void setup_ups_sensitivity(UPSINFO *ups);
-extern void setup_ups_wakeup(UPSINFO *ups);
-extern void setup_ups_sleep(UPSINFO *ups);
-extern void setup_ups_lo_xfer(UPSINFO *ups);
-extern void setup_ups_hi_xfer(UPSINFO *ups);
-extern void setup_ups_chargepoint(UPSINFO *ups);
-extern void setup_ups_alarm(UPSINFO *ups);
-extern void setup_ups_lowbatt_delay(UPSINFO *ups);
-extern void setup_ups_selftest(UPSINFO *ups);
-
-extern const char *get_model_from_oldfwrev(const char *s);
-extern void get_apc_capabilities(UPSINFO *ups);
-extern void read_extended(UPSINFO *ups);
-extern void setup_extended(UPSINFO *ups);
 
 /* In apcnis.c */
 extern void do_server(UPSINFO *ups);
@@ -165,23 +111,17 @@ extern void stat_open(UPSINFO *ups);
 extern int stat_close(UPSINFO *ups, int fd);
 extern void stat_print(UPSINFO *ups, const char *fmt, ...);
 
-
 /* In apcevents.c */
 extern int trim_eventfile(UPSINFO *ups);
 extern int output_events(int sockfd, FILE *events_file);
 
-
 /* In apcreports.c */
 extern void clear_files(void);
 extern int log_status(UPSINFO *ups);
-
-extern void timer_reports(int sig);
 extern void do_reports(UPSINFO *ups);
 
 /* In apcsignal.c */
 extern void init_signals(void (*handler) (int));
-extern void init_thread_signals(void);
-extern void restore_signals(void);
 
 /* In newups.c */
 extern UPSINFO *new_ups(void);
@@ -204,7 +144,7 @@ extern void _write_unlock(const char *file, int line, UPSINFO *ups);
 extern int start_thread(UPSINFO *ups, void (*action) (UPSINFO * ups),
    const char *proctitle, char *argv0);
 extern int execute_command(UPSINFO *ups, UPSCOMMANDS cmd);
-extern void wait_for_termination(int serial_pid);
+extern void clean_threads(void);
 
 /* In apclog.c */
 extern void log_event(const UPSINFO *ups, int level, const char *fmt, ...);
@@ -224,7 +164,6 @@ extern void generic_error_exit(const char *fmt, ...);
 /* In asys.c */
 int avsnprintf(char *str, size_t size, const char *format, va_list ap);
 int asnprintf(char *str, size_t size, const char *fmt, ...);
-char *afgets(char *s, int size, FILE *fd);
 void *amalloc(size_t size);
 void *arealloc(void *buf, size_t size);
 void *acalloc(size_t size1, size_t size2);
@@ -234,9 +173,6 @@ size_t strlcpy(char *dst, const char *src, size_t size);
 #ifndef HAVE_STRLCAT
 size_t strlcat(char *dst, const char *src, size_t size);
 #endif
-
-/* In apcwinipc.c */
-int winioctl(int fd, int func, int *addr);
 
 /* In sleep.c */
 #ifndef HAVE_NANOSLEEP
